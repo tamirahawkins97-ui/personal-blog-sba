42baadb92a3cb39fa47497417c301467f5338175
Tiona Hawkins <tionahawkins@Tionas-MacBook-Pro.local>
2026-08-02 14:05:19 -0400

    Updates added

---
+++ script.js
@@ -0,0 +1,5 @@
+const blogForm = document.getElementById("blogForm");
+const titleInput = document.getElementById("title");
+const titleError = document.getElementById("titleError");
+const submitBtn = document.querySelector("button");
+const 
